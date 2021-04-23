52e7ad1e44860120498c7cb27e55c429b661f321
Max <max.machnik@gmail.com>
2021-04-23 10:11:02 +0200

    Analize refactor

--- graphql/resolvers/analize.js
+++ graphql/resolvers/analize.js
@@ -1,62 +1,56 @@
-const Company = require('../../models/Company');
+const Company = require("../../models/Company");
 
 const addMonths = ({ date, monthsCount }) => {
-	const updatedDate = new Date(date.setMonth(date.getMonth() + monthsCount));
-	return updatedDate;
+    const updatedDate = new Date(date.setMonth(date.getMonth() + monthsCount));
+    return updatedDate;
 };
 
-const calculateReturn = ({ start, end }) => {
-	return ((end.closePrice - start.closePrice) / start.closePrice) * 100;
+const calculateReturn = ({ base, effect }) => {
+    return ((effect - base) / base) * 100;
 };
 
-const findExtreams = ({ array, startDate, endDate }) => {
-	const filteredArr = array.filter(
-		day => day.date > startDate && day.date <= endDate
-	);
-	const minCourses = filteredArr
-		.map(day => day.minPrice)
-		.sort((a, b) => a - b);
-	const maxCourses = filteredArr
-		.map(day => day.maxPrice)
-		.sort((a, b) => a - b);
-	const min = minCourses[0];
-	const max = maxCourses[minCourses.length - 1];
-	return { min, max };
+const findExtreams = ({ filteredArr }) => {
+    const minCourses = filteredArr
+        .map((day) => day.minPrice)
+        .sort((a, b) => a - b);
+    const maxCourses = filteredArr
+        .map((day) => day.maxPrice)
+        .sort((a, b) => a - b);
+    const min = minCourses[0];
+    const max = maxCourses[minCourses.length - 1];
+    return { min, max };
+};
+
+const analizeForXMonths = ({ startDate, dataArray, monthsCount }) => {
+    const endDate = addMonths({ date: startDate, monthsCount });
+    const filteredArr = dataArray.filter(
+        (day) => day.date >= startDate && day.date <= endDate
+    );
+    const basePrice = filteredArr[0].closePrice;
+    const { min, max } = findExtreams({ filteredArr });
+    return {
+        endReturn: calculateReturn({
+            base: basePrice,
+            effect: filteredArr[filteredArr.length - 1],
+        }),
+        min: calculateReturn({ base: basePrice, effect: min }),
+        max: calculateReturn({ base: basePrice, effect: max }),
+    };
 };
 
 module.exports = {
-	analizeEffects: async ({ companyId, baseDate }) => {
-		const company = await Company.findById(companyId);
-		if (!company) {
-			console.log('No company found for data: ', companyId);
-		} else {
-			const basicDate = company.data.find(
-				day => day.date.toString() === new Date(baseDate).toString()
-			);
-			const plusOneMonthData = company.data.find(
-				day =>
-					day.date.toString() ===
-					addMonths({
-						monthsCount: 1,
-						date: new Date(baseDate),
-					}).toString()
-			);
-			const oneMonthExreams = findExtreams({
-				array: company.data,
-				startDate: new Date(baseDate),
-				endDate: addMonths({
-					monthsCount: 1,
-					date: new Date(baseDate),
-				}),
-			});
-			console.log(oneMonthExreams);
-			console.log(
-				(oneMonthExreams.min - basicDate.closePrice) /
-					basicDate.closePrice
-			);
-			console.log(
-				calculateReturn({ start: basicDate, end: plusOneMonthData })
-			);
-		}
-	},
+    analizeEffects: async ({ companyId, baseDate }) => {
+        const company = await Company.findById(companyId);
+        if (!company) {
+            console.log("No company found for data: ", companyId);
+        } else {
+            console.log(
+                analizeForXMonths({
+                    startDate: new Date(baseDate),
+                    dataArray: company.data,
+                    monthsCount: 1,
+                })
+            );
+        }
+    },
 };
